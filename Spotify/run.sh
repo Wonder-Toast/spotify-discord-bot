80dfb46c5498ae3f86fce15aedcefbf6ad4a7c0f
Gavin Wainwright <gav.wainwright@gmail.com>
2017-05-02 13:14:37 -0400

    Create run.sh

---
+++ Spotify/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# If you have issues running this try this code
+# sudo chmod 777 run.sh
+# It is most likely that it will not work when you first use it
+echo If you get any issues running this script then read the help in the file
+node app.js
